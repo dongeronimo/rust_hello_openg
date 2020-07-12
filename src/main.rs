805a5df0aa5315e840a38f4499377556ee105d5e
luciano <luciano.geronimo.fnord@gmail.com>
2020-07-12 12:43:19 -0300

    Refactored the shader program creation. Now shaders.rs has no opengl/string/file operations, they are in shader_utils and others

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod shaders;
 mod sdl_utils;
 mod main_loop;
 mod utils;
+mod shader_utils;
 fn main() {
     let sdl = sdl_utils::init_sdl();
     let video_subsystem = sdl_utils::init_video_subsystem(&sdl, sdl_utils::OpenGLProfile::Core4_5);
--- src/shaders.rs
+++ src/shaders.rs
@@ -1,7 +1,8 @@
 use gl;
 use std;
 use std::ffi::{CStr};
-pub use crate::utils::create_whitespace_cstring_with_len;
+pub use crate::shader_utils::{shader_from_source, create_shader,compile_shader, create_program, 
+    attach_shaders_to_program};
 
 pub struct Shader {
     id: gl::types::GLuint,
@@ -29,86 +30,22 @@ impl Drop for Shader{
     }
 }
 
-fn shader_from_source(source: &CStr, kind: gl::types::GLenum) -> Result<gl::types::GLuint, String>{
-    let id = create_shader(kind);
-    match compile_shader(id, &source){
-        Ok(_)=> {
-            return Ok(id);
-        },
-        Err(msg)=>{
-            return Err(msg);
-        }
-    }
-}
-/*The first step of the shader creation process is to tell opengl that you want a new shader and get the opengl id of this
-newly-minted shader. This id will be used whenever you manipulate the shader.*/
-fn create_shader(kind: gl::types::GLenum)->gl::types::GLuint{
-    unsafe{gl::CreateShader(kind)}
-}
-fn compile_shader(id:gl::types::GLuint, source: &CStr)->Result<String, String>{
-    let mut success: gl::types::GLint = 1;
-    unsafe {
-        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
-        gl::CompileShader(id);
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-    }
-    if success == 0 {
-        let (error, len) = allocate_string_buffer_for_opengl_error(id);
-        unsafe {
-            gl::GetShaderInfoLog(
-                id,
-                len,
-                std::ptr::null_mut(),
-                error.as_ptr() as *mut gl::types::GLchar,
-            );
-        }
-        //retorna o erro
-        return Err(error.to_string_lossy().into_owned())
-    }
-    else{
-        Ok(String::from("shader compiled ok"))
-    }
-}
 
-fn allocate_string_buffer_for_opengl_error(id:gl::types::GLuint)-> (std::ffi::CString,gl::types::GLint)  {
-    let mut len: gl::types::GLint = 0;
-    unsafe {gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);} 
-    let string_buffer = create_whitespace_cstring_with_len(len as usize);
-    (string_buffer, len)
-}
+
 pub struct Program {
     id: gl::types::GLuint,
 }
 
 impl Program {
     pub fn from_shaders(shaders: &[Shader])->Result<Program, String> {
-        let program_id = unsafe {gl::CreateProgram()};
-        //Vincula os shaders ao programa
-        for shader in shaders {
-            unsafe {gl::AttachShader(program_id, shader.id())};
-        }
-        //Linkagem
-        unsafe {gl::LinkProgram(program_id);}
-        let mut success: gl::types::GLint = 1;
-        unsafe{gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);}
-        //Lida com erro
-        if success == 0 {
-            let mut len: gl::types::GLint = 0;
-            unsafe {gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);}
-            let error = create_whitespace_cstring_with_len(len as usize);
-            unsafe{gl::GetProgramInfoLog(program_id, len, std::ptr::null_mut(), error.as_ptr() as *mut gl::types::GLchar);}
-            Err(error.to_string_lossy().into_owned())
-        }
-        //Lida com o sucesso
-        else{
-            for shader in shaders {
-                unsafe {gl::DetachShader(program_id, shader.id());}
-            }
-            Ok(Program {id: program_id})
+        let program_id = create_program();
+        match attach_shaders_to_program(program_id, shaders){
+            Err(err_msg)=> return Err(err_msg),
+            Ok(_)=> return Ok(Program{id:program_id}),
         }
     }
-    pub fn id(&self){
-        self.id;
+    pub fn id(&self)->gl::types::GLuint{
+        return self.id;
     }
     pub fn set_used(&self){
         unsafe{gl::UseProgram(self.id);}
@@ -116,6 +53,6 @@ impl Program {
 }
 impl Drop for Program {
     fn drop(&mut self){
-        unsafe{gl::DeleteProgram(self.id);}
+        unsafe{gl::DeleteProgram(self.id());}
     }
 }
