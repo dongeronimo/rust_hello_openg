376a02613f92ed7dc7589affa318e454d4c09352
luciano <luciano.geronimo.fnord@gmail.com>
2020-07-13 17:08:12 -0300

    creating the scene crate. It models the objects in a scene

---
+++ model_scene/src/lib.rs
@@ -0,0 +1,30 @@
+extern crate cgmath;
+extern crate uuid;
+
+pub struct Scene {
+    
+}
+
+pub struct Transform {
+
+}
+
+pub struct Camera {
+
+}
+
+pub struct Light {
+
+}
+
+pub struct Object {
+
+} 
+
+pub struct Mesh {
+
+}
+
+pub struct Material {
+    
+}
