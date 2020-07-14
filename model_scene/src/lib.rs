e7bbcb458b36939dfd0e0eb8f1fa432422ac4f76
luciano <luciano.geronimo.fnord@gmail.com>
2020-07-13 22:24:56 -0300

    Moved the model_scene::Transform to its own file

--- model_scene/src/lib.rs
+++ model_scene/src/lib.rs
@@ -1,52 +1,97 @@
 extern crate cgmath;
 extern crate uuid;
+pub mod transform;
 use std::collections::HashMap;
 use uuid::Uuid;
+use crate::transform::Transform;
+//------------------------------------------------------------------------------
 
-pub struct Transform {
-
+//------------------------------------------------------------------------------
+pub trait Identity {
+    fn id(&self)->String;
 }
-impl Transform{
-    pub fn new()->Transform{
-        return Transform{}
+//------------------------------------------------------------------------------
+pub struct Camera{
+    id: String,
+    transform: Transform,
+    //TODO: Coisas especificas de camera
+}
+impl Camera{
+    pub fn new() -> Camera{
+        let id = Uuid::new_v4().to_string();
+        let transform = Transform::new();
+        return Camera{
+            id: id,
+            transform: transform,
+        }
     }
+}
+impl Identity for Camera{
+    fn id(&self) -> std::string::String { self.id.clone() }
+}
+//------------------------------------------------------------------------------
+pub struct Light{
+    id:String,
+    transform: Transform,
+    //TODO: Coisas especificas de luz
+}
+impl Light {
+    pub fn new() -> Light{
+        let id = Uuid::new_v4().to_string();
+        let transform = Transform::new();
+        return Light{
+            id: id,
+            transform: transform,
+        }
+    }
+}
+impl Identity for Light{
+    fn id(&self) -> std::string::String { self.id.clone() }
 }
 //------------------------------------------------------------------------------
 pub struct Object {
     id:String,
     transform: Transform,
-    parent_id: String,
-    children: Vec<String>
+    //TODO: Coisas espeficas de Objetos 3d
 }
 impl Object{
-    pub fn new()->Object{
-        let transform = Transform::new();
+    pub fn new() -> Object{
         let id = Uuid::new_v4().to_string();
+        let transform = Transform::new();
         return Object{
             id: id,
             transform: transform,
-            parent_id: "".to_string(),
-            children: Vec::new(),
         }
     }
-    pub fn id(&self)->String{
-        self.id.clone()
-    }
+}
+impl Identity for Object{
+    fn id(&self) -> std::string::String { self.id.clone() }
 }
 //------------------------------------------------------------------------------
-pub struct World {
-    objects: HashMap<String, Object>,
-    root_object_id:String,
+pub enum WorldObject {
+    Camera(Camera),
+    Light(Light),
+    Object(Object),
+}
+struct World {
+    objects: HashMap<String, WorldObject>,
+    root_object_id: String,
+    camera_id: String,
 }
 impl World{
-    pub fn new()->World{
-        let root_object = Object::new();
-        let root_object_id = root_object.id(); 
-        let mut map = HashMap::new();
-        map.insert(root_object.id(), root_object);
+    pub fn new() -> World{
+        let root = Object::new();
+        let root_id = root.id();
+        let camera = Camera::new();
+        let camera_id = camera.id();
+        let mut map: HashMap<String, WorldObject> = HashMap::new();
+        
+        map.insert(root.id(), WorldObject::Object(root));
+        map.insert(camera.id(), WorldObject::Camera(camera));
         return World{
             objects: map,
-            root_object_id: root_object_id
+            root_object_id: root_id,
+            camera_id: camera_id
         }
     }
 }
